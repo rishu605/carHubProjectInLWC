d27ddcef9760828d1a1a5f1fff9fb692e093c682
rishabh r gupta <rishabh.r.gupta@apiseroinc.onmicrosoft.com>
2022-01-14 23:16:07 +0530

    Project Structure Setup

---
+++ carHubProjectInLWC/force-app/main/default/lwc/carFilter/carFilter.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class CarFilter extends LightningElement {
+    filters = {
+        searchKey:'',
+        maxPrice:999999
+    }
+    handleSearchKeyChange() {
+
+    }
+
+    handleMaxPriceChange() {
+
+    }
+}
